55ed3970ea7178941291858f08def309beea693a
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-07-20 00:51:13 +0300

    Create a function that will populate the display area

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let firstNumber = '';
 let secondNumber = '';
 let operator = '';
+let displayValue = '';
 
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
@@ -35,3 +36,18 @@ function operate(operator, firstNumber, secondNumber) {
             return divide(firstNumber, secondNumber);
     }
 }
+
+function activateDigits() {
+    let digits = document.querySelectorAll(".digit");
+    let displayArea = document.querySelector(".display-area");
+    digits.forEach((digit) => {
+        digit.addEventListener("click", () => {
+            if (displayValue.length < 20) {
+                displayValue += digit.textContent;
+                displayArea.textContent = displayValue;
+            }
+        })
+    })
+}
+
+activateDigits();
