1dcdbbaa512818a392df39f060cc3ada23866a31
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-07-19 19:36:40 +0300

    Create the operate function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let firstNumber = "";
-let secondNumber = "";
-let operator = "";
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
 
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
@@ -18,4 +18,20 @@ function divide(firstNumber, secondNumber) {
     if (secondNumber === 0) {
         return "CAN NOT DIVIDE BY ZERO";
     } return firstNumber / secondNumber;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+    }
 }
