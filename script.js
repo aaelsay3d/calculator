9c9a0e0e9d2df9e7748f4b5ba05c2afd250870d2
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-07-19 19:12:37 +0300

    Create script.js file and add functions for operators

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    if (secondNumber === 0) {
+        return "CAN NOT DIVIDE BY ZERO";
+    } return firstNumber / secondNumber;
+}
